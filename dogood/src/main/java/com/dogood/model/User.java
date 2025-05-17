d0df3b872712ed4aa4b74cf6897a58b6c8f58ba6
raveendra11 <Gurthuledu@1>
2025-05-17 11:47:13 -0500

    Inception

---
+++ dogood/src/main/java/com/dogood/model/User.java
@@ -0,0 +1,28 @@
+package com.dogood.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        DONOR, ADMIN, BENEFICIARY
+    }
+}
---
+++ dogood/src/main/java/com/dogood/service/UserService.java
@@ -0,0 +1,22 @@
+package com.dogood.service;
+
+import com.dogood.model.User;
+import com.dogood.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User registerUser(User user) {
+        System.out.println("Registering user: " + user);
+        return userRepository.save(user);
+    }
+
+}
