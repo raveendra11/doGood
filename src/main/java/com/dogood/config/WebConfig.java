873b5b8b618ff3431a35d8db5cd65642b9e83418
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-05-27 17:59:43 -0500

    Update WebConfig.java

--- src/main/java/com/dogood/config/WebConfig.java
+++ src/main/java/com/dogood/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     	registry.addMapping("/**")
         .allowedOrigins(
             "http://localhost:3000", 
-            "https://dogood-web-482150591633.us-central1.run.app/"
+            "https://dogood-web-482150591633.us-central1.run.app"
         )
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
