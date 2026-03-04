05dc28be6519832059042a7c90b014033204c3a8
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2026-03-04 13:18:52 -0600

    Update WebConfig.java

--- src/main/java/com/dogood/config/WebConfig.java
+++ src/main/java/com/dogood/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
     	registry.addMapping("/**")
         .allowedOrigins(
-            "http://localhost:3000", //Local
-            "https://dogood-web-23738934914.us-central1.run.app", //Cloud run
-            "https://wedogood.help" //domain-map
-            "https://34.128.167.227" //GKE
+            "http://localhost:3000",
+            "https://dogood-web-23738934914.us-central1.run.app",
+            "https://wedogood.help", 
+            "https://34.128.167.227"
         )
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
